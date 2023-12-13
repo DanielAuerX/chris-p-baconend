7ac08e66105f0de9f53b7028fe4fa531e6bd1b3d
Daniel Auer <d.auer@eos-ts.com>
2023-12-13 11:28:18 +0100

    chatbot returns next prompt

--- src/main/java/com/chrispbacon/chrispbaconend/chatbot/Chatbot.java
+++ src/main/java/com/chrispbacon/chrispbaconend/chatbot/Chatbot.java
@@ -6,7 +6,9 @@ import com.chrispbacon.chrispbaconend.util.Guard;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 @Component
@@ -15,10 +17,20 @@ public class Chatbot {
 
     private final PromptRepository promptRepository;
     private final ChoiceRepository choiceRepository;
+    private final Map<UUID, UUID> choiceToPromptMapping;
 
     public Chatbot(PromptRepository promptRepository, ChoiceRepository choiceRepository) {
         this.promptRepository = promptRepository;
         this.choiceRepository = choiceRepository;
+        this.choiceToPromptMapping = getChoiceToPromptMapping();
+    }
+
+    private Map<UUID, UUID> getChoiceToPromptMapping() {
+        HashMap<UUID, UUID> choiceToPromptMapping = new HashMap<>();
+        choiceToPromptMapping.put(UUID.fromString("550e8400-e29b-41d4-a716-446655440011"), UUID.fromString("550e8400-e29b-41d4-a716-446655440001"));
+        choiceToPromptMapping.put(UUID.fromString("550e8400-e29b-41d4-a716-446655440012"), UUID.fromString("550e8400-e29b-41d4-a716-446655440001"));
+        choiceToPromptMapping.put(UUID.fromString("550e8400-e29b-41d4-a716-446655440013"), UUID.fromString("550e8400-e29b-41d4-a716-446655440001"));
+        return choiceToPromptMapping;
     }
 
     public PromptDto getInitialPrompt() {
@@ -29,4 +41,11 @@ public class Chatbot {
         return new PromptDto(initialPrompt.getId(), initialPrompt.getText(), initialChoices);
     }
 
+    public PromptDto getNextPrompt(UUID previousChoiceId) {
+        UUID nextPromptId = choiceToPromptMapping.get(previousChoiceId);
+        Prompt initialPrompt = promptRepository.findById(nextPromptId).orElseThrow();
+        List<Choice> initialChoices = choiceRepository.findAllByPromptId(nextPromptId);
+        Guard.againstEmptyList(initialChoices, "No choices found for prompt " + nextPromptId);
+        return new PromptDto(initialPrompt.getId(), initialPrompt.getText(), initialChoices);
+    }
 }
--- src/main/java/com/chrispbacon/chrispbaconend/controller/ChatbotController.java
+++ src/main/java/com/chrispbacon/chrispbaconend/controller/ChatbotController.java
@@ -5,8 +5,11 @@ import com.chrispbacon.chrispbaconend.chatbot.PromptDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/chrispbacon/support")
@@ -21,5 +24,10 @@ public class ChatbotController {
     @GetMapping()
     public ResponseEntity<PromptDto> getInitialPrompt() {
         return ResponseEntity.ok(chatbot.getInitialPrompt());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PromptDto> getNextPrompt(@PathVariable UUID id) {
+        return ResponseEntity.ok(chatbot.getNextPrompt(id));
     }
 }
